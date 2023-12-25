ea6c302e14d0604acb5383838692f3f599dee970
DanielMakokha <danielmakokhan@gmail.com>
2023-12-25 22:55:52 +0300

    db-xpress con

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 // 4)START SERVER
-
+const mongoose=require('mongoose');
 const dotenv=require('dotenv');
 
-dotenv.config({path: './config.env'})
+dotenv.config({path: './config.env'});
+
+const DB=process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
+mongoose
+// .connect(process.env.DATABASE_LOCAL,{//lOCAL DATABASE
+
+//HOSTED DATABASE
+.connect(DB,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useFindAndModify:false
+})
+.then(con => {
+   console.log(con.connections);
+   console.log('DB connected successfully'); 
+})
 const app=require('./app');
 
 console.log(app.get('env'));
