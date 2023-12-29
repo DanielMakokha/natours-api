d5cf670e20e6ef13577faa7a273070c558c6c332
DanielMakokha <danielmakokhan@gmail.com>
2023-12-29 23:34:35 +0300

    models refactor

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,21 +1,5 @@
-const fs=require('fs');
+const Tour=require('./../models/tourModel')
 
-const tours=JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-)
-
-exports.checkID=(req,res,next,val) =>{
-    console.log(`Tour ID is ${val}`);
-    if(req.params.id>tours.length){
-        return res.status(404).json({
-            status:'fail',
-            message:'Invalid ID'
-        
-            
-        })
-    } 
-    next();
-};
 
 exports.checkBody=(req,res,next) =>{
     if(!req.body.name || !req.body.price){
@@ -30,40 +14,34 @@ exports.checkBody=(req,res,next) =>{
 exports.getAllTours=(req,res) =>{
     res.status(200).json({
         status:'success',
-        result:tours.length,
-        data: {
-            tours: tours
-        }
+        // result:tours.length,
+        // data: {
+        //     tours: tours
+        // }
     })
 }
 exports.getTour=(req,res) =>{
     console.log(req.params);
     const id=req.params.id * 1;
     
-    const tour= tours.find(el => el.id === id)
-    res.status(200).json({
-        status:'success',
-        data: {
-            tour
-        }
-    });
+    // const tour= tours.find(el => el.id === id)
+    // res.status(200).json({
+    //     status:'success',
+    //     data: {
+    //         tour
+    //     }
+    // });
 }
 
 exports.createTour=(req,res) =>{
     // console.log(req.body);
-    const newId=tours[tours.length-1].id +1;
-    const newTour=Object.assign({id: newId},req.body);
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err =>{
-        res.status(201).json({
-            status:"success",
-            data:{
-                tours:newTour
-            }
-        })
+    res.status(201).json({
+        status:"success",
+        // data:{
+        //     tours:newTour
+        // }
     })
-    // res.send('Done');
-}
+};
 
 exports.updateTour=(req,res) =>{  
     
